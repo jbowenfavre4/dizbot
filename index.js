f464a53fb18dad5a22c782c69242a47c013f82d3
Bowen Revill <bowenrevill4@gmail.com>
2022-04-15 14:29:57 -0600

    bot

---
+++ index.js
@@ -0,0 +1,67 @@
+require('dotenv').config();
+const https = require('https')
+const { Client, Intents } = require('discord.js')
+const { LoremIpsum }  = require('lorem-ipsum')
+const lorem = new LoremIpsum({
+    sentencesPerParagraph: {
+        max: 8,
+        min: 4
+    },
+    wordsPerSentence: {
+        max: 16,
+        min: 4
+    }
+})
+const giveMeAJoke = require('give-me-a-joke')
+const puppeteer = require('puppeteer')
+const client = new Client(
+    { intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] }
+)
+client.on('ready', function(e) {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+client.login(process.env.BOT_TOKEN)
+client.on('message', async msg => {
+    if (msg.author.bot) return
+    if (msg.content === 'hey dizbot') {
+        msg.reply('hey')
+    } else if (msg.content === 'dizbot help') {
+        msg.reply('"hey dizbot": say hello\n"dizbot lorem": produces lorem ipsum words\n"dizbot virus": gives number of current cases in Utah county\n"dizbot dadjoke": tells a joke')
+    } else if (msg.content === 'dizbot lorem') {
+        msg.reply(lorem.generateWords(10))
+    } else if (msg.content === 'dizbot virus') {
+        (async () => {
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto(process.env.COVID_SITE);
+            element = await page.waitForSelector('#kEEOx > div:nth-child(2) > div.wsV78c > div > div:nth-child(1) > table > tbody > tr > td:nth-child(1) > div.h5Hgwe > div > span')
+            number = await element.evaluate(el => el.textContent)
+            msg.reply(`${number} cases today in Utah County`)
+            await browser.close();
+          })();
+    } else if (msg.content === 'dizbot dadjoke') {
+        giveMeAJoke.getRandomDadJoke (function(joke) {
+            msg.reply(joke)
+        })
+    } else if (msg.content === 'dizbot btc') {
+        let options = {
+            "method": "GET",
+            "hostname": "rest.coinapi.io",
+            "path": "/v1/exchangerate/BTC/USD",
+            "headers": {'X-CoinAPI-Key': process.env.CRYPTO_KEY}
+        };
+        var request = https.request(options, res => {
+            let data = ''
+            res.on('data', (chunk) => {
+                data = data + chunk.toString()
+            })
+            res.on('end', () => {
+                const body = JSON.parse(data)
+                msg.reply(`${body.asset_id_base} is currently trading at ${Math.round(body.rate*100)/100} ${body.asset_id_quote}`)
+            })
+        })
+        request.end()
+        
+    }
+})
+
